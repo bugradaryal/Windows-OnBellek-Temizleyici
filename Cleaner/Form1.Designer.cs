6a50f9847c9763b21949d436089164f0d84fc87d
Buğra Daryal <bugradaryal@hotmail.com>
2022-02-28 14:05:37 +0300

    .

---
+++ Cleaner/Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿
+namespace Cleaner
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.backgroundWorker2 = new System.ComponentModel.BackgroundWorker();
+            this.backgroundWorker3 = new System.ComponentModel.BackgroundWorker();
+            this.backgroundWorker4 = new System.ComponentModel.BackgroundWorker();
+            this.backgroundWorker5 = new System.ComponentModel.BackgroundWorker();
+            this.button4 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.PowderBlue;
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.Green;
+            this.button1.FlatAppearance.BorderSize = 3;
+            this.button1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.LightSkyBlue;
+            this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.White;
+            resources.ApplyResources(this.button1, "button1");
+            this.button1.Name = "button1";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.PowderBlue;
+            this.button2.FlatAppearance.BorderColor = System.Drawing.Color.Green;
+            this.button2.FlatAppearance.BorderSize = 3;
+            this.button2.FlatAppearance.MouseDownBackColor = System.Drawing.Color.LightSkyBlue;
+            this.button2.FlatAppearance.MouseOverBackColor = System.Drawing.Color.White;
+            resources.ApplyResources(this.button2, "button2");
+            this.button2.Name = "button2";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.PowderBlue;
+            this.button3.FlatAppearance.BorderColor = System.Drawing.Color.Green;
+            this.button3.FlatAppearance.BorderSize = 3;
+            this.button3.FlatAppearance.MouseDownBackColor = System.Drawing.Color.LightSkyBlue;
+            this.button3.FlatAppearance.MouseOverBackColor = System.Drawing.Color.White;
+            resources.ApplyResources(this.button3, "button3");
+            this.button3.Name = "button3";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.PowderBlue;
+            this.button5.FlatAppearance.BorderColor = System.Drawing.Color.Green;
+            this.button5.FlatAppearance.BorderSize = 3;
+            this.button5.FlatAppearance.MouseDownBackColor = System.Drawing.Color.LightSkyBlue;
+            this.button5.FlatAppearance.MouseOverBackColor = System.Drawing.Color.White;
+            resources.ApplyResources(this.button5, "button5");
+            this.button5.Name = "button5";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
+            // 
+            // backgroundWorker2
+            // 
+            this.backgroundWorker2.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker2_DoWork);
+            this.backgroundWorker2.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker2_RunWorkerCompleted);
+            // 
+            // backgroundWorker3
+            // 
+            this.backgroundWorker3.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker3_DoWork);
+            this.backgroundWorker3.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker3_RunWorkerCompleted);
+            // 
+            // backgroundWorker4
+            // 
+            this.backgroundWorker4.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker4_DoWork);
+            this.backgroundWorker4.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker4_RunWorkerCompleted);
+            // 
+            // backgroundWorker5
+            // 
+            this.backgroundWorker5.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker5_DoWork);
+            this.backgroundWorker5.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker5_RunWorkerCompleted);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.PowderBlue;
+            this.button4.FlatAppearance.BorderColor = System.Drawing.Color.Green;
+            this.button4.FlatAppearance.BorderSize = 3;
+            this.button4.FlatAppearance.MouseDownBackColor = System.Drawing.Color.LightSkyBlue;
+            this.button4.FlatAppearance.MouseOverBackColor = System.Drawing.Color.White;
+            resources.ApplyResources(this.button4, "button4");
+            this.button4.Name = "button4";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // pictureBox1
+            // 
+            resources.ApplyResources(this.pictureBox1, "pictureBox1");
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DodgerBlue;
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button5;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker2;
+        private System.ComponentModel.BackgroundWorker backgroundWorker3;
+        private System.ComponentModel.BackgroundWorker backgroundWorker4;
+        private System.ComponentModel.BackgroundWorker backgroundWorker5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Cleaner/Form1.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.ComponentModel;
+using System.Drawing;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace Cleaner
+{
+    public partial class Form1 : Form
+    {
+        DialogResult dialog;
+        bool button = false;
+        string pc_username = Environment.UserName;
+        string system_disc = Path.GetPathRoot(Environment.GetFolderPath(Environment.SpecialFolder.System));
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public void messagebox(string metin, string hata, string baslik)
+        {
+            var box = new Form() { Size = new Size(0, 0) };
+            //Task.Delay(TimeSpan.FromSeconds(3)).ContinueWith((t) => box.Close(), TaskScheduler.FromCurrentSynchronizationContext());
+            if (hata == "")
+                MessageBox.Show(box, metin, baslik, MessageBoxButtons.OK, MessageBoxIcon.Information);
+            else
+                MessageBox.Show(box, metin + "\n Hata:" + hata, baslik,MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+        enum RecycleFlags : uint
+        {
+            SHRB_NOCONFIRMATION = 0x00000001, // Don't ask confirmation
+            SHRB_NOPROGRESSUI = 0x00000002, // Don't show any windows dialog
+            SHRB_NOSOUND = 0x00000004 // Don't make sound, ninja mode enabled :v
+        }
+        [DllImport("Shell32.dll", CharSet = CharSet.Unicode)]
+        static extern uint SHEmptyRecycleBin(IntPtr hwnd, string pszRootPath, RecycleFlags dwFlags);
+        public void file_islem(DirectoryInfo konum)
+        {
+            try
+            {
+                foreach (FileInfo file in konum.GetFiles())
+                {
+                    try
+                    {
+                        file.Delete();
+                    }
+                    catch (Exception) { }
+                }
+                foreach (DirectoryInfo dir in konum.GetDirectories())
+                {
+                    try
+                    {
+                        dir.Delete(true);
+                    }
+                    catch (Exception) { }
+                } 
+            }
+            catch (DirectoryNotFoundException hata) 
+            {
+                messagebox("Hata İle Karşılaşıldı!!", hata.ToString(), "Cleaner");
+            }
+        }
+        private void button1_Click(object sender, EventArgs e) // hepsi
+        {
+            dialog = MessageBox.Show("Silme İşlemini Gerçekleştireceğinize Eminmisiniz?", "Cleaner", MessageBoxButtons.YesNo, MessageBoxIcon.Exclamation);
+            if (dialog == DialogResult.Yes)
+            {
+                button = true;
+                label1.Text = "All...";
+                label1.Visible = true;
+                backgroundWorker1.RunWorkerAsync();
+            }
+        }
+        private void button2_Click(object sender, EventArgs e) // temp
+        {
+            dialog = MessageBox.Show("Silme İşlemini Gerçekleştireceğinize Eminmisiniz?","Cleaner", MessageBoxButtons.YesNo, MessageBoxIcon.Exclamation);
+            if(dialog == DialogResult.Yes)
+            {
+                label1.Text = "Temp...";
+                label1.Visible = true;
+                backgroundWorker2.RunWorkerAsync();
+            }
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            dialog = MessageBox.Show("Silme İşlemini Gerçekleştireceğinize Eminmisiniz?", "Cleaner", MessageBoxButtons.YesNo, MessageBoxIcon.Exclamation);
+            if (dialog == DialogResult.Yes)
+            {
+                label1.Text = "%Temp%...";
+                label1.Visible = true;
+                backgroundWorker3.RunWorkerAsync();
+            }
+        } //%temp%
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string pc_username = Environment.UserName;
+            dialog = MessageBox.Show("Silme İşlemini Gerçekleştireceğinize Eminmisiniz?", "Cleaner", MessageBoxButtons.YesNo, MessageBoxIcon.Exclamation);
+            if (dialog == DialogResult.Yes)
+            {
+                label1.Text = "Cache...";
+                label1.Visible = true;
+                backgroundWorker4.RunWorkerAsync();
+            }
+        }  //internet cache
+        private void button5_Click(object sender, EventArgs e)
+        {
+            dialog = MessageBox.Show("Silme İşlemini Gerçekleştireceğinize Eminmisiniz?", "Cleaner", MessageBoxButtons.YesNo, MessageBoxIcon.Exclamation);
+            if (dialog == DialogResult.Yes)
+            {
+                label1.Text = "Trash...";
+                label1.Visible = true;
+                backgroundWorker5.RunWorkerAsync();
+            }
+        }  //çöp kutusu
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            label1.Text = "Starting...";
+        }
+        private void backgroundWorker2_DoWork(object sender, DoWorkEventArgs e)
+        {
+            file_islem(new DirectoryInfo(system_disc + @"Windows\Temp"));
+        }
+        private void backgroundWorker3_DoWork(object sender, DoWorkEventArgs e)
+        {
+            file_islem(new DirectoryInfo(system_disc + @"Users\" + pc_username + @"\AppData\Local\Temp"));
+        }
+        private void backgroundWorker4_DoWork(object sender, DoWorkEventArgs e)
+        {
+            file_islem(new DirectoryInfo(Environment.GetFolderPath(Environment.SpecialFolder.InternetCache)));
+        }
+        private void backgroundWorker5_DoWork(object sender, DoWorkEventArgs e)
+        {
+            try
+            {
+                uint IsSuccess = SHEmptyRecycleBin(IntPtr.Zero, null, RecycleFlags.SHRB_NOCONFIRMATION);
+            }
+            catch (Exception hata) 
+            {
+                messagebox("Hata İle Karşılaşıldı!!", hata.ToString(), "Cleaner");
+            }
+        }
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            backgroundWorker2.RunWorkerAsync();
+        }
+        private void backgroundWorker2_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            label1.Text = "Temp Cleared...";
+            if (button == false)
+            {
+                messagebox("İşlem Tamamlandı...", "", "Cleaner");
+                label1.Visible = false;
+            }
+            else
+                backgroundWorker3.RunWorkerAsync();
+        }
+        private void backgroundWorker3_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            label1.Text = "%Temp% Cleared...";
+            if (button == false)
+            {
+                messagebox("İşlem Tamamlandı...", "", "Cleaner");
+                label1.Visible = false;
+            }
+            else
+                backgroundWorker4.RunWorkerAsync();
+        }
+        private void backgroundWorker4_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            label1.Text = "Cache Cleared...";
+            if (button == false)
+            {
+                messagebox("İşlem Tamamlandı...", "", "Cleaner");
+                label1.Visible = false;
+            }
+            else
+            {
+                label1.Text = "All Cleared Except Trash...";
+                backgroundWorker5.RunWorkerAsync();
+            }
+        }
+        private void backgroundWorker5_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            label1.Text = "Trash Cleared...";
+            if (button == true)
+                label1.Text = "All Cleared...";
+            messagebox("İşlem Tamamlandı...", "", "Cleaner");
+            label1.Visible = false;
+            button = false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label1.Visible = false;
+        }
+    }
+}
+
